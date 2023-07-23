44f8e169d3e35f9312bdd0a2f955616c24476b30
Berke Atalay <berke.atalay@beakwise.com>
2023-07-23 12:50:50 +0300

    faet: explore new location

--- command_explore.go
+++ command_explore.go
@@ -1,15 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func commandExplore(cfg *config, commandArgs []string) error {
-	fmt.Println()
-	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage:")
-	fmt.Println()
-	for _, cmd := range getCommands() {
-		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	if len(commandArgs) == 0 {
+		return errors.New("Please write a location name to explore")
 	}
+	location := commandArgs[0]
+	fmt.Println(fmt.Sprintf(`Exploring %s `, location))
+	results, err := cfg.pokeapiClient.Explore(location)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Found Pokemon: ")
+	for _, encounter := range results.PokemonEncounters {
+		fmt.Println(encounter.Pokemon.Name)
+	}
+
 	fmt.Println()
 	return nil
 }
--- internal/pokeapi/location.go
+++ internal/pokeapi/location.go
@@ -2,47 +2,52 @@ package pokeapi
 
 import (
 	"encoding/json"
+	"errors"
 	"io"
 	"net/http"
 )
 
 // ListLocations -
-func (c *Client) explore(locationName string) (RespShallowLocations, error) {
+func (c *Client) Explore(locationName string) (RespLocationDetails, error) {
 	url := baseURL + "/location-area/" + locationName
 
 	cache, isFound := c.cache.Get(url)
 	if isFound {
-		locationsResp := RespShallowLocations{}
+		locationsResp := RespLocationDetails{}
 		err := json.Unmarshal(cache, &locationsResp)
 		if err != nil {
-			return RespShallowLocations{}, err
+			return RespLocationDetails{}, err
 		}
 		return locationsResp, nil
 	}
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return RespShallowLocations{}, err
+		return RespLocationDetails{}, err
 	}
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
-		return RespShallowLocations{}, err
+		return RespLocationDetails{}, err
 	}
 	defer resp.Body.Close()
 
+	if resp.StatusCode == 404 {
+		return RespLocationDetails{}, errors.New("Requested Location is not found. Please make sure that location is same as in list")
+	}
+
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return RespShallowLocations{}, err
+		return RespLocationDetails{}, err
 	}
 
 	c.cache.Add(url, data)
 
-	locationsResp := RespShallowLocations{}
-	err = json.Unmarshal(data, &locationsResp)
+	locationResp := RespLocationDetails{}
+	err = json.Unmarshal(data, &locationResp)
 	if err != nil {
-		return RespShallowLocations{}, err
+		return RespLocationDetails{}, err
 	}
 
-	return locationsResp, nil
+	return locationResp, nil
 }
